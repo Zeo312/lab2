df0a8d09787733c1694f1c68742829d5957f697c
Shilpa <z5170043@cse.unsw.edu.au>
2017-08-06 14:39:43 +1000

    Added fizzbuzz function with no params

--- hello.py
+++ hello.py
@@ -17,5 +17,17 @@ v=(input("Enter number"))
 
 fizzbuzz(v)
 
+def fizzbuzz():
+        for i in range(1,101):
+                if(i%3==0 and i%5==0):
+                        print("FizzBuzz")
+                elif(i%3==0):
+			print("Fizz")
+		elif(i%5==0):
+			print("Buzz")
+		else:
+			print(i)
+                        
+
 
 
